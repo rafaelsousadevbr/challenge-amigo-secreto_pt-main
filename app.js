904baf2dde4e1b3a3d4202facfddc57a8ad2b57f
rafaelsousadevbr <rafael@sousalima.dev.br>
2025-09-21 22:58:45 -0300

    Criada a lista de amigos para armazenar os nomes

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programa. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+//Cria a lista de amigos para armazenar os nomes
+const listaDeAmigos = [];
+
+
+
+
